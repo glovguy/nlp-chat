42ef553cd0a4cac472eb6ed490b530df06bb5686
Karl <karlsmith@bouzou.com>
2016-08-20 15:42:35 -0400

    Modifications to try and make the app run on heroku

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http'),
 	fs = require('fs'),
 	validator = require('validator'),
+	express = require('express'),
 	port = (process.env.PORT || 5000);
+
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX) )
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 var app = http.createServer(function (request, response) {
 	fs.readFile("client.html", 'utf-8', function (error, data) {
@@ -17,9 +22,10 @@ io.sockets.on('connection', function(socket) {
 	socket.on('message_to_server', function(data) {
 		var escaped_message = validator.escape(data["message"]);
 		io.sockets.emit("message_to_client",{ message: escaped_message });
+		console.log(escaped_message);
 		sleep(3000).then(() => {
 			io.sockets.emit("message_from_nlp",{ message: "hi there!" });
-		})
+		});
 	});
 });
 
