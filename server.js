089e6fb9155cfcdf6e90c83dc12d870b3db0598f
Karl <karlsmith@bouzou.com>
2016-10-17 22:11:11 -0400

    Interface with chat api is now using json, now supports both api and server being locally hosted instances, and some tidying up

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ var http = require('http'),
 	fs = require('fs'),
 	validator = require('validator'),
 	express = require('express'),
-	port = (process.env.PORT || 5005);
+	request = require('request');
+
+if (process.env.PROD) {
+	port = (process.env.PORT);
+} else {
+	port = (5005);
+}
+
+if (process.env.PROD) {
+	resource_name = 'http://language-games.herokuapp.com'
+} else {
+	resource_name = 'http://localhost:5000'
+}
 
 const url = require('url');
 
@@ -22,35 +34,8 @@ app.get('/', function(request, response) {
 	});
 });
 
-app.get('/bootstrap.html', function(request, response) {
-	fs.readFile("mybootstrap.html", 'utf-8', function (error, data) {
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write(data);
-		response.end();
-	});
-});
-
 server.listen(port);
 console.log("running... navigate to http://localhost:".concat(port.toString()));
-
-callback = function(response) {
-	var str = '';
-
-	//another chunk of data has been recieved, so append it to `str`
-	response.on('data', function (chunk) {
-		str += chunk;
-	});
-
-	//the whole response has been received, so we just print it out here
-	response.on('end', function () {
-		console.log(str=='True');
-		if (str == 'True') {
-			nlp_transmit("Why do you want that?");
-		} else {
-			nlp_transmit("Tell me more...");
-		};
-	});
-}
 
 nlp_transmit = function(message) {
 	nlp_message = message;
@@ -64,18 +49,28 @@ io.sockets.on('connection', function(socket) {
 		io.sockets.emit("message_to_client",{ message: escaped_message });
 		console.log("Someone said: " + escaped_message);
 
+		api_url = url.parse(resource_name + '/intention?text=' + escaped_message );
+
+		request(
+			{
+				json: true,
+				uri: api_url
+			}, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+			    console.log("RESPONSE_BODY:");
+				console.log(body);
+				console.log(body["intention_with_which"]);
+				if (body["intention_with_which"] == true) {
+					nlp_transmit("Why do you want that?");
+				} else {
+					nlp_transmit("Tell me more...");
+				};
+			} else {
+				console.log("SOMETHING WENT WRONG");
+				console.log(error + response + body);
+			}
 		
+		})
 
-		api_url = url.parse('http://language-games.herokuapp.com/intention?text=' + escaped_message)
-		var options = {
-		  host: api_url.host,
-		  path: api_url.path
-		};
-
-		http.request(options, callback).end();
 	});
 });
-
-function sleep (time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
